9d6e482c2b0f4dcf1d3042d527bb31b2fd20da61
robert-virtual <robertocastill63@gmail.com>
2021-12-22 11:20:50 -0600

    basic login

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,39 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 import Head from "next/head";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Notes App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="#">
-            Notes App
-          </a>
-        </h1>
+      <nav className="flex fixed w-full p-4 shadow-md top-0 justify-between">
+        <span>Nice Notes</span>
+        <button
+          onClick={signIn}
+          className="text-white bg-blue-500 p-2 rounded-sm"
+        >
+          Login
+        </button>
+      </nav>
+      <main className="flex flex-col items-center justify-center w-full px-20 text-center">
+        {!session ? (
+          <h1 className="text-6xl font-bold">
+            Welcome to{" "}
+            <a className="text-blue-600" href="#">
+              Notes App
+            </a>
+          </h1>
+        ) : (
+          <h1 className="text-6xl font-bold">
+            Welcome{" "}
+            <a className="text-blue-600" href="#">
+              {session.user.name}
+            </a>
+          </h1>
+        )}
       </main>
     </div>
   );
